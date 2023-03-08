2964cce81638914c0a9a5e728884e6539d8e4cf9
Madan <madangopal16072000@gmail.com>
2023-03-08 16:33:31 +0530

    using rtk query

---
+++ src/components/User/usersApiSlice.js
@@ -0,0 +1,16 @@
+// import apiSlice from "../../api/apiSlice";
+
+// const usersApiSlice = apiSlice.injectEndpoints({
+//   endpoints: (builder) => ({
+//     login: builder.mutation({
+//       query: data => ({
+//         url: "/login",
+//         method: "POST",
+//         body: { ...data },
+//       }),
+//     }),
+//   }),
+// });
+
+// export const { useLoginMutation } = usersApiSlice;
+// export default usersApiSlice;
